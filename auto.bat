146f2c5fc11589a168a04e1640d135dcd5739c98
Parygina Anastasiya <paryginanastya@yandex.com>
2024-05-21 11:03:16 +0300

    Add a batch file.

---
+++ auto.bat
@@ -0,0 +1,5 @@
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe" Parygina_Project.sln /build Debug
+"C:\Program Files\doxygen\bin\doxygen.exe" config
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" distribution1.iss
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" distribution2.iss
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" distribution3.iss
